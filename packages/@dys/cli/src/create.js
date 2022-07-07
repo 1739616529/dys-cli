bded9048e35b7c1cd5b6e0c0587b83133c0e3863
1739616529 <1739616529@qq.com>
2022-07-07 19:49:43 +0800

    统一风格  添加  生成模板

--- packages/@dys/cli/src/create.js
+++ packages/@dys/cli/src/create.js
@@ -1,65 +1,76 @@
-const { cwd, gitStorm } = require('../dysconfig.js')
-const url_delay = require('../util/url_delay.js')
-const inquirer = require('inquirer')
-const { Listr } = require('listr2')
-const { join } = require('path')
-
+const { cwd, tmpDir } = require("../dysconfig.js");
+const url_delay = require("../util/url_delay.js");
+const inquirer = require("inquirer");
+const { Listr } = require("listr2");
+const { join } = require("path");
+const { ensureDirSync, ensureFileSync } = require("fs-extra");
+const { dir_ok, get_github_storm, download_file, uncompress } = require("../util/tools");
+const { mv } = require("shelljs");
 async function create_tmpl() {
-	const prompt_lsit = [
-		{
-			type: 'input',
-			message: '项目名. （Project name）',
-			name: 'name',
-			validate: function (val) {
-				// 如果为空
-				if (val === '')
-					return `请输入项目名. (Please enter a project name)`
-				// 规范 文件夹名称
-				if (val.match(/(\\|\/|\:|\*|\?|\"|\<|\>|\|)/g))
-					return '请填写正确的文件夹名称. (Please fill in the correct folder name)'
+    const prompt_lsit = [
+        {
+            type: "input",
+            message: "项目名. （Project name）",
+            name: "name",
+            validate: function (val) {
+                // 如果为空
+                if (val === "") return `请输入项目名. (Please enter a project name)`;
+                // 规范 文件夹名称
+                if (val.match(/(\\|\/|\:|\*|\?|\"|\<|\>|\|)/g))
+                    return "请填写正确的文件夹名称. (Please fill in the correct folder name)";
 
-				// 文件夹是否已存在
-				if (dir_ok(join(cwd, val)))
-					return '文件夹已存在. (Folder already exists)'
+                // 文件夹是否已存在
+                if (dir_ok(join(cwd, val))) return "文件夹已存在. (Folder already exists)";
 
-				return true
-			},
-		},
-		{
-			type: 'list',
-			message: '框架. （Frame）',
-			name: 'frame',
-			choices: ['React', 'Vue', 'Vanilla'],
-		},
-		{
-			type: 'confirm',
-			message: '启用 Typescript. (Typescript Supper)',
-			name: 'typescript',
-		},
-	]
-	async function online_test(url = 'https://www.baidu.com') {
-		const rask = new Listr([
-			{
-				title: '网络测速. (Network testing)',
-				task: async (ctx, task) => {
-					const data = await url_delay(url)
-					if (data.ok)
-						task.title = `测试完成. (test is finished)      github.com ${data.ping}ms`
-					if (!data.ok)
-						throw new Error(
-							'网络不通畅. (Poor network connection)'
-						)
-				},
-			},
-		])
+                return true;
+            },
+        },
+        {
+            type: "list",
+            message: "框架. （Frame）",
+            name: "frame",
+            choices: ["React", "Vue", "Vanilla"],
+        },
+        {
+            type: "confirm",
+            message: "启用 Typescript. (Typescript Supper)",
+            name: "typescript",
+        },
+    ];
+    async function online_test(url = "https://www.baidu.com") {
+        const rask = new Listr([
+            {
+                title: "网络测速. (Network testing)",
+                task: async (ctx, task) => {
+                    const data = await url_delay(url);
+                    if (data.ok)
+                        task.title = `测试完成. (test is finished)      github.com ${data.ping}ms`;
+                    if (!data.ok) throw new Error("网络不通畅. (Poor network connection)");
+                },
+            },
+        ]);
 
-		await rask.run()
-	}
+        await rask.run();
+    }
 
-	await online_test()
-	inquirer.prompt(prompt_lsit).then((res) => {
-		console.log(res)
-	})
+    // await online_test();
+    inquirer.prompt(prompt_lsit).then(async (res) => {
+        // 获取  项目仓库信息
+        const { path, fileName, storm_name } = get_github_storm(res);
+
+        // 创建目录
+        ensureDirSync(tmpDir);
+        const file_path = join(tmpDir, fileName);
+
+        // 如果不存在 则下载
+        if (!ensureFileSync(file_path)) await download_file({ path, savePath: tmpDir, fileName });
+
+        // 解压 到当前目录
+        uncompress(file_path, cwd);
+
+        // 重命名
+        mv(storm_name, res.name);
+    });
 }
 
-module.exports = create_tmpl
+module.exports = create_tmpl;
--- packages/@dys/cli/util/tools.js
+++ packages/@dys/cli/util/tools.js
@@ -1,54 +1,84 @@
-const fs = require('fs-extra')
-const { exec, which } = require('shelljs')
-const https = require('https')
+const fs = require("fs-extra");
+const { exec, which } = require("shelljs");
+const request = require("request");
+const config = require("../dysconfig");
+const admZip = require("adm-zip");
 module.exports = {
-	dir_ok(path) {
-		try {
-			fs.accessSync(path, fs.constants.F_OK)
-			return true
-		} catch (err) {
-			return false
-		}
-	},
-	instead_model(name) {
-		return which(name)
-	},
+    dir_ok(path) {
+        try {
+            fs.accessSync(path, fs.constants.F_OK);
+            return true;
+        } catch (err) {
+            return false;
+        }
+    },
+    instead_model(name) {
+        return which(name);
+    },
 
-	check_version(name = '@dys/cli') {
-		return new Promise((resolve, reject) => {
-			let data = ''
-			https.get(
-				`https://mirrors.cloud.tencent.com/npm/${name}/latest`,
-				(res) => {
-					if (res.statusCode === 404)
-						return reject(
-							new Error(
-								'Not fond this package'
-							)
-						)
-					res.on('data', (body) => {
-						data += body
-					})
-				}
-			)
-				.on('error', reject)
-				.on('close', () => {
-					resolve(
-						data && JSON.parse(data).version
-					)
-				})
-		})
-	},
+    check_version(name = "@dys/cli") {
+        return new Promise((resolve, reject) => {
+            let data = "";
+            https
+                .get(`https://mirrors.cloud.tencent.com/npm/${name}/latest`, (res) => {
+                    if (res.statusCode === 404) return reject(new Error("Not fond this package"));
+                    res.on("data", (body) => {
+                        data += body;
+                    });
+                })
+                .on("error", reject)
+                .on("close", () => {
+                    resolve(data && JSON.parse(data).version);
+                });
+        });
+    },
 
-	compare_version(preVersion = '', lastVersion = '') {
-		var sources = preVersion.split('.')
-		var dests = lastVersion.split('.')
-		var maxL = Math.max(sources.length, dests.length)
-		for (let i = 0; i < maxL; i++) {
-			let pre = parseInt(sources[i] || 0)
-			let last = parseInt(dests[i] || 0)
-			if (pre < last) return true
-		}
-		return false
-	},
-}
+    compare_version(preVersion = "", lastVersion = "") {
+        var sources = preVersion.split(".");
+        var dests = lastVersion.split(".");
+        var maxL = Math.max(sources.length, dests.length);
+        for (let i = 0; i < maxL; i++) {
+            let pre = parseInt(sources[i] || 0);
+            let last = parseInt(dests[i] || 0);
+            if (pre < last) return true;
+        }
+        return false;
+    },
+
+    get_github_storm(option) {
+        // react-vite-electron/archive/refs/heads/main.zip
+        const { gitStorm } = config;
+        const projects = gitStorm[option.typescript ? "ts" : "js"];
+        const storm_name = projects[option.frame];
+        const barch = projects.barch;
+        const file_name = `${storm_name}-${barch}.zip`;
+        const path = `${gitStorm.baseUrl}/${storm_name}/archive/refs/heads/${barch}.zip`;
+
+        return {
+            path,
+            storm_name,
+            fileName: file_name,
+        };
+    },
+
+    download_file(option) {
+        const { path, savePath, fileName } = option;
+        if (!path) return;
+        return new Promise((resolve) => {
+            const file = fs.createWriteStream(`${savePath}/${fileName}`);
+            const res = request(path).pipe(file);
+            res.on("finish", resolve);
+        });
+    },
+    uncompress(zipFile, destFolder) {
+        var zip = new admZip(zipFile);
+
+        // var zipEntries = zip.getEntries();
+        // for (var i = 0; i < zipEntries.length; i++) {
+        //     var entry = zipEntries[i];
+        //     entry.entryName = iconv.decode(entry.rawEntryName, "gbk");
+        // }
+
+        zip.extractAllTo(destFolder, true);
+    },
+};
