2dfd776fd7ca24d45e2d7797b6f465caea372ae9
1739616529 <1739616529@qq.com>
2022-07-07 19:48:27 +0800

    脚手架配置

--- packages/@dys/cli/dysconfig.js
+++ packages/@dys/cli/dysconfig.js
@@ -1,46 +1,57 @@
-module.exports = {
-	cwd: process.cwd(),
-	version: require('./package.json').version,
-
-	gitStorm: {
-		ts: {
-			Vue: '',
-			React: '',
-			Vanilla: '',
-		},
-		Vanilla: {
-			Vue: '',
-			React: '',
-			Vanilla: '',
-		},
-	},
-
-	origin: {
-		npm: {
-			name: 'npm',
-			key: 'npm',
-			value: 'https://registry.npmjs.org/',
-		},
-		yarn: {
-			name: 'yarn',
-			key: 'yarn',
-			value: 'https://registry.yarnpkg.com/',
-		},
-		tencent: {
-			name: 'tencent',
-			key: 'tencent',
-			value: 'https://mirrors.cloud.tencent.com/npm/',
-		},
-		cnpm: {
-			name: 'cnpm',
-			key: 'cnpm',
-			value: 'https://r.cnpmjs.org/',
-		},
-
-		npmMirror: {
-			name: 'npmMirror (淘宝 taobao.org)',
-			key: 'npmMirror',
-			value: 'https://registry.npmmirror.com/',
-		},
-	},
+function get_tmp_dir() {
+    const dir = {
+        win32: process.env.LOCALAPPDATA,
+        linux: "/tmp",
+        darwin: "/tmp",
+    };
+    return `${dir[process.platform]}/dys`;
 }
+module.exports = {
+    cwd: process.cwd(),
+    version: require("./package.json").version,
+    tmpDir: get_tmp_dir(),
+    gitStorm: {
+        baseUrl: "https://github.com/1739616529",
+        ts: {
+            Vue: "",
+            React: "react-vite-electron",
+            Vanilla: "",
+            barch: "main",
+        },
+        js: {
+            Vue: "",
+            React: "",
+            Vanilla: "",
+            barch: "js",
+        },
+    },
+
+    origin: {
+        npm: {
+            name: "npm",
+            key: "npm",
+            value: "https://registry.npmjs.org/",
+        },
+        yarn: {
+            name: "yarn",
+            key: "yarn",
+            value: "https://registry.yarnpkg.com/",
+        },
+        tencent: {
+            name: "tencent",
+            key: "tencent",
+            value: "https://mirrors.cloud.tencent.com/npm/",
+        },
+        cnpm: {
+            name: "cnpm",
+            key: "cnpm",
+            value: "https://r.cnpmjs.org/",
+        },
+
+        npmMirror: {
+            name: "npmMirror (淘宝 taobao.org)",
+            key: "npmMirror",
+            value: "https://registry.npmmirror.com/",
+        },
+    },
+};
