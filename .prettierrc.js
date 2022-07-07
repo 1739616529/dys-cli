d82b19b03162d06538fc5e0072a7a9b85d75cff5
1739616529 <1739616529@qq.com>
2022-07-07 19:19:30 +0800

    格式化 代码

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 160, // 最大换行宽度
+    singleQuote: false, // 使用单引号替代双引号
+    semi: true, // 行尾分号
+    printWidth: 100, // 最大行宽 100
+    tabWidth: 4, // tab 2 个空格
+    trailingComma: "all", //对象 数组 最后的 逗号
+    bracketSameLine: false, // html 标签的尖括号是否换行
+    arrowParens: "always", // 箭头函数参数是否带圆括号
+};
