8de7f42eb4a91530cf27c6eb6df1583f420ff201
1739616529 <1739616529@qq.com>
2022-07-09 10:11:02 +0800

    修改 连接创建

--- scripts/cliLink.ts
+++ scripts/cliLink.ts
@@ -3,7 +3,7 @@ import { exec, cd, rm, echo } from "shelljs";
 import npm_conf from "npm-conf";
 const cwd = process.cwd();
 import { pack_dir } from "./config";
-
+const npm_config = npm_conf();
 const cli_dir = `${cwd}/${pack_dir}/cli`;
 const cli_pkg = require(`${cli_dir}/package.json`);
 
@@ -12,7 +12,7 @@ echo("unlink cli");
 exec(`npm unlink ${cli_pkg.name}`);
 
 echo("delete cli cmd");
-const npm_prefix = npm_conf.get("prefix");
+const npm_prefix = npm_config.get("prefix");
 rm(`${npm_prefix}/dys*`);
 rm("-rf", `${npm_prefix}/node_modules/${cli_pkg.name}*`);
 
