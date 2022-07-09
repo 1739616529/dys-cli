1fc2fd0b79d60f3ff787ed0307daca2f78935897
1739616529 <1739616529@qq.com>
2022-07-10 00:08:56 +0800

    rust打包config

--- scripts/config.ts
+++ scripts/config.ts
@@ -2,6 +2,7 @@ export const pack_dir = "packages/@dinyaso";
 export interface Order_Item {
     order: string;
     release: string;
+    target: string;
 }
 export interface CARGO_BUILD {
     win32: Order_Item;
@@ -10,15 +11,18 @@ export interface CARGO_BUILD {
 }
 export const cargo_build: CARGO_BUILD = {
     win32: {
-        order: "cargo build --target=x86_64-pc-windows-msvc",
-        release: "",
-    },
-    darwin: {
-        order: "cargo build ",
+        order: "cargo build --release --target=x86_64-pc-windows-msvc",
+        target: "x86_64-pc-windows-msvc",
         release: "",
     },
     linux: {
-        order: "cargo build --target=",
+        order: "cargo build --release --target=x86_64-unknown-linux-musl",
+        target: "x86_64-unknown-linux-musl",
+        release: "",
+    },
+    darwin: {
+        order: "cargo build --release --target=x86_64-apple-darwin",
+        target: "x86_64-apple-darwin",
         release: "",
     },
 };
