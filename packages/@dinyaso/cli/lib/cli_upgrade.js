cd775698c3856fa40765d109807f8f5101502ccc
1739616529 <1739616529@qq.com>
2022-07-10 00:08:07 +0800

    代码风格统一

--- packages/@dinyaso/cli/lib/cli_upgrade.js
+++ packages/@dinyaso/cli/lib/cli_upgrade.js
@@ -1,9 +1,9 @@
-const { check_version, compare_version } = require('../util/tools')
-const { version } = require('../dysconfig')
+const { check_version, compare_version } = require("../util/tools");
+const { version } = require("../dysconfig");
 function cli_upgrade() {
-	return check_version('vue').then((res) => {
-		return compare_version(version, res)
-	})
+    return check_version("vue").then((res) => {
+        return compare_version(version, res);
+    });
 }
 
-module.exports = cli_upgrade
+module.exports = cli_upgrade;
