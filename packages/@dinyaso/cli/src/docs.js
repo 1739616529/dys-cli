486d4af31aa0026823104110ee9bacda4304b464
1739616529 <1739616529@qq.com>
2022-07-10 00:08:16 +0800

    docs 模块

--- packages/@dinyaso/cli/src/docs.js
+++ packages/@dinyaso/cli/src/docs.js
@@ -1,2 +1,21 @@
-function docs() {}
+const { join } = require("path");
+const { exec } = require("shelljs");
+const serve = require("../util/serve");
+const { serveProt } = require("../dysconfig");
+function docs() {
+    const webview_name = {
+        win32: "docs.exe",
+        darwin: "docs",
+        linux: "docs",
+    };
+    const webview = join(__dirname, "../docs/", webview_name[process.platform]);
+    const server = serve(serveProt, join(__dirname, "../docs/"));
+    process.on("exit", () => {
+        server.close();
+    });
+    process.on("exit", () => {
+        console.log("exit");
+    });
+    // exec(webview);
+}
 module.exports = docs;
