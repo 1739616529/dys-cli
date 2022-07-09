aff3d89a350cfa1ce1620bccefa8438871761b11
1739616529 <1739616529@qq.com>
2022-07-09 10:13:03 +0800

    发版

---
+++ scripts/publish.ts
@@ -0,0 +1,14 @@
+import { cd, cp, exec, mv } from "shelljs";
+import { pack_dir, cargo_build, Order_Item } from "./config";
+const cwd = process.cwd();
+const pack_path = `${cwd}/${pack_dir}`;
+
+// 打包 rust
+// const cargo_order_item: Order_Item = cargo_build[process.platform];
+// cd(`${pack_path}/webview`);
+// exec(cargo_order_item.order);
+
+// 打包 vitepress
+cd(cwd);
+exec("npm run build docs");
+cp("-rf", `${pack_path}/docs/docs/.vitepress/dist`, `${pack_path}/cli/docs`);
