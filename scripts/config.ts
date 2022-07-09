266e22a54c0f1eb63be7dbfacc09741cda621e7d
1739616529 <1739616529@qq.com>
2022-07-09 10:12:54 +0800

    script配置项

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,1 +1,24 @@
 export const pack_dir = "packages/@dinyaso";
+export interface Order_Item {
+    order: string;
+    release: string;
+}
+export interface CARGO_BUILD {
+    win32: Order_Item;
+    darwin: Order_Item;
+    linux: Order_Item;
+}
+export const cargo_build: CARGO_BUILD = {
+    win32: {
+        order: "cargo build --target=x86_64-pc-windows-msvc",
+        release: "",
+    },
+    darwin: {
+        order: "cargo build ",
+        release: "",
+    },
+    linux: {
+        order: "cargo build --target=",
+        release: "",
+    },
+};
