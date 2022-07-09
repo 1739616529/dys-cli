251a9eec5754885911c09557ff98c7488edf5aae
1739616529 <1739616529@qq.com>
2022-07-10 00:08:32 +0800

    统一处理 进程退出操作

--- packages/@dinyaso/cli/src/index.js
+++ packages/@dinyaso/cli/src/index.js
@@ -7,31 +7,37 @@ const docs = require("./docs");
 const program = require("commander");
 const { version } = require("../dysconfig.js");
 (function () {
-    program.version(version);
+    function run_command() {
+        program.version(version);
 
-    // 运行 安装
-    program.command("i").description("安装依赖. (Install node_modules)").action(install);
+        // 运行 安装
+        program.command("i").description("安装依赖. (Install node_modules)").action(install);
 
-    // 运行 安装
-    program.command("docs").description("脚手架文档. (Cli docs)").action(docs);
+        // 运行 安装
+        program.command("docs").description("脚手架文档. (Cli docs)").action(docs);
 
-    // 创建项目
-    program.command("create").description("创建项目. (create project)").action(create_tmpl);
+        // 创建项目
+        program.command("create").description("创建项目. (create project)").action(create_tmpl);
 
-    // 修改 源
-    program
-        .command("origin")
-        .description("修改 npm/yarn/pnpm 源. (change npm/yarn/pnpm download origin)")
-        .action(change_origin);
+        // 修改 源
+        program
+            .command("origin")
+            .description("修改 npm/yarn/pnpm 源. (change npm/yarn/pnpm download origin)")
+            .action(change_origin);
 
-    // 更新脚手架
-    program.command("upgrade").description("更新脚手架. (Upgrade cli)").action(upgrade);
+        // 更新脚手架
+        program.command("upgrade").description("更新脚手架. (Upgrade cli)").action(upgrade);
 
-    // 运行 脚本
-    program
-        .command("run <script>")
-        .description("需要运行的命令 相当于 npm run (Input order)")
-        .action(running);
+        // 运行 脚本
+        program
+            .command("run <script>")
+            .description("需要运行的命令 相当于 npm run (Input order)")
+            .action(running);
 
-    program.parse(process.argv);
+        program.parse(process.argv);
+    }
+
+    run_command();
+    function process_exit() {}
+    process.on("exit", process_exit);
 })();
