1e673e3d787c0b37c3568a5148839e0e3bdbc76e
1739616529 <1739616529@qq.com>
2022-07-10 00:09:25 +0800

    rust

--- packages/@dinyaso/webview/src/main.rs
+++ packages/@dinyaso/webview/src/main.rs
@@ -1,14 +1,18 @@
+use std::env;
 use web_view::*;
-
 fn main() {
-    builder()
-        .title(" cli docs")
-        .content(Content::Url("https://www.baidu.com"))
-        .size(800, 600)
-        .resizable(true)
-        .debug(true)
-        .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
-        .run()
-        .unwrap();
+    let args: Vec<String> = env::args().collect();
+    let url = &args[1];
+    if url.len() > 0 {
+        builder()
+            .title("cli docs")
+            .content(Content::Url(url))
+            .size(800, 600)
+            .resizable(true)
+            // .debug(true)
+            .user_data(())
+            .invoke_handler(|_webview, _arg| Ok(()))
+            .run()
+            .unwrap();
+    }
 }
