e368c04c677459337d60efec22c2560d5c406911
1739616529 <1739616529@qq.com>
2022-07-10 02:33:15 +0800

    linux 依赖安装脚本

---
+++ packages/@dinyaso/webview/pkg.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+sudo apt install -y pkg-config
+sudo apt install -y libglib2.0-dev
+sudo apt install -y libcairo2-dev
+sudo apt install -y libatk-bridge2.0
+sudo apt install -y libsoup*
+sudo apt install -y libwebkit2gtk-4.0-37
+sudo apt install -y  libwebkit2gtk-4.0-dev
