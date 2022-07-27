46de9bdd9b161ccc54fd16da8d4a9b3f2c05dfc2
1739616529 <1739616529@qq.com>
2022-07-27 22:44:00 +0800

    工具函数

--- packages/@dinyaso/cli/util/serve.js
+++ packages/@dinyaso/cli/util/serve.js
@@ -1,18 +1,18 @@
 const http = require("http");
 const fs = require("fs-extra");
 const { join } = require("path");
+const { dir_ok } = require("./tools");
 module.exports = function serve(port, dir) {
-    function format_url(req) {
-        let url = req.url;
+    function format_url(url) {
         if (url === "/") url = "index.html";
-        if (url === "/favicon.ico") return false;
-        return url;
+        const full_url = join(dir, url);
+        if (dir_ok(full_url) === false) return false;
+        return full_url;
     }
     const server = http.createServer((req, res) => {
-        const url = format_url(req);
+        const url = format_url(req.url);
         if (url === false) return;
-        const file_path = join(dir, url);
-        fs.createReadStream(file_path).pipe(res);
+        fs.createReadStream(url).pipe(res);
     });
     server.listen(port);
     return server;
