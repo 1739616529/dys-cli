f4d6ed791549b12774aea324fa53aa59e48d4270
1739616529 <1739616529@qq.com>
2022-07-27 22:43:50 +0800

    prettier 配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,7 +4,7 @@ module.exports = {
     semi: true, // 行尾分号
     printWidth: 100, // 最大行宽 100
     tabWidth: 4, // tab 2 个空格
-    trailingComma: "all", //对象 数组 最后的 逗号
+    trailingComma: "es5", //对象 数组 最后的 逗号
     bracketSameLine: false, // html 标签的尖括号是否换行
     arrowParens: "always", // 箭头函数参数是否带圆括号
 };
