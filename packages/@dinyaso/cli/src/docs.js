fc45680e6df310a1fff22ae9ecd8e64df1e7d9c8
Code pianist <77950104+1739616529@users.noreply.github.com>
2022-07-29 13:49:40 +0800

    docs 添加  webview

--- packages/@dinyaso/cli/src/docs.js
+++ packages/@dinyaso/cli/src/docs.js
@@ -1,6 +1,8 @@
 const { join } = require("path");
-const { exec } = require("shelljs");
+const { exec } = require("child_process");
 const serve = require("../util/serve");
+const boxne = require("boxen");
+const chalk = require("chalk");
 const { serveProt } = require("../dysconfig");
 function docs() {
     const webview_name = {
@@ -13,9 +15,26 @@ function docs() {
     process.on("exit", () => {
         server.close();
     });
-    process.on("exit", () => {
-        console.log("exit");
-    });
-    // exec(webview);
+
+    const url = `http://127.0.0.1:${serveProt}`;
+    exec(`${webview} ${url}`);
+    console.log(
+        boxne("local url: " + chalk.blue(url), {
+            textAlignment: "center",
+            width: 20,
+            borderColor: "blue",
+            borderStyle: "round",
+            padding: {
+                top: 1,
+                bottom: 1,
+                left: 9,
+                right: 9,
+            },
+            margin: {
+                top: 1,
+                bottom: 1,
+            },
+        })
+    );
 }
 module.exports = docs;
